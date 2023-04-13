0f2c6151b6fb1438cd4b72efae038b78c4735d89
Shubham Patel <71441598+shubham24680@users.noreply.github.com>
2023-04-13 18:22:32 +0530

    Update Rough.c

--- Rough.c
+++ Rough.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include <conio.h>
+
+int[] count() {
+    int k = 0;
+    for(int i = 0; i < inputDist.size; i++) {
+        if(start <= inputDist.data[i] && inputDist.data[i] <= end) {
+            answer.data[k++] = inputDist.data[i]; 
+        }
+    }
+}
+
 
 int main(){
     int l, r, n;
-    scanf("%d %d %d", &l, &r, &n);
-
-        
+    scanf("%d %d %d", &l, &r, &n);    
 }
